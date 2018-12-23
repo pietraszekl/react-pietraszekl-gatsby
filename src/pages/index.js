f24d00880627966ee7e722394a66f8b1dca67838
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2018-12-23 23:37:20 +0100

    responsove fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,59 +14,58 @@ const IndexPage = () => (
 
     <StaticQuery
       query={graphql`
-      query PersonDataQuery {
-        site {
-          siteMetadata {
-            aboutData {
-           person {
-             photo
-             name
-             title
-             intro
-             specialities
-             btnText
-             connect {
-               icon
-               link
-               name
-             }
-           }
-            
-           recommendations {
-            id
-            text
-            author
-           }
-           skills {
-             header
-             intro
-             list
-           }
-           projects {
-             title
-             intro
-             list {
-               id
-               logo
-             }
-           }
+        query PersonDataQuery {
+          site {
+            siteMetadata {
+              aboutData {
+                person {
+                  photo
+                  name
+                  title
+                  intro
+                  specialities
+                  btnText
+                  connect {
+                    icon
+                    link
+                    name
+                  }
+                }
+                recommendations {
+                  id
+                  text
+                  author
+                }
+                skills {
+                  header
+                  intro
+                  list
+                }
+                projects {
+                  title
+                  intro
+                  list {
+                    id
+                    logo
+                    name
+                  }
+                }
+              }
             }
           }
         }
-      }
-    `}
+      `}
       render={data => (
         <>
           <Hero person={data.site.siteMetadata.aboutData.person} />
           <Skills skills={data.site.siteMetadata.aboutData.skills} />
           <Portfolio projects={data.site.siteMetadata.aboutData.projects} />
-          <Recommendations recommendations={data.site.siteMetadata.aboutData.recommendations} />
+          <Recommendations
+            recommendations={data.site.siteMetadata.aboutData.recommendations}
+          />
         </>
       )}
     />
-
-
-
   </Layout>
 )
 
