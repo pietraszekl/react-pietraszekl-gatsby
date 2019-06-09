fb105df0200daf9c08d454d914e2ee8461a24d4a
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2019-06-09 10:29:53 +0200

    introducing cypressio for e2e tests

---
+++ cypress/integration/homepage.spec.js
@@ -0,0 +1,13 @@
+context('Homepage', function() {
+  beforeEach(function() {
+    cy.visit('/')
+  })
+  it('Title should include "Senior Frontend', () => {
+    // https://on.cypress.io/title
+    cy.title().should('include', 'Senior Frontend ')
+  })
+  it('Title should contain "Lukasz Pietraszek"', () => {
+    // https://on.cypress.io/title
+    cy.title().should('include', 'Lukasz Pietraszek')
+  })
+})
