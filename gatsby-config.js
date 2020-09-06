89934e613ac47fbe40e4f3d2693c009d78960c82
Lukasz Pietraszek <525704+pietraszekl@users.noreply.github.com>
2020-09-06 17:05:48 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,20 +14,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-12509524-5',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-      },
-    },
-    {
       resolve: `gatsby-source-graphql`,
       options: {
         fieldName: process.env.GRAPHCMS_API_FIELDNAME,
